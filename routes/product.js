e926b2960602c33bb2d0628cbef2eb50ab8ac2b7
JeonghakLee <wjdgkr0527@naver.com>
2022-07-24 23:19:40 +0900

    불필요한 로그 제거

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
 const { isLoggedIn } = require("./middlewares");
+const router = express.Router();
+const multer = require("../src/multers");
 const Product = require("../models/product");
 const ProductImg = require("../models/productImg");
-const router = express.Router();
-const upload = require("../src/multers");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -30,21 +30,6 @@ router.get("/detail", async (req, res, next) => {
   }
 });
 
-router.post(
-  "/images",
-  isLoggedIn,
-  upload.array("productImgs", 10),
-  async (req, res) => {
-    const images = req.files;
-    if (images) {
-      const locations = await images.map((img) => img.location);
-      console.log(locations);
-      return res.status(201).send(locations);
-    }
-    res.status(400).send("이미지가 없습니다.");
-  }
-);
-
 router.get("/seller", isLoggedIn, async (req, res, next) => {
   try {
     const products = await Product.findAll({
@@ -56,14 +41,12 @@ router.get("/seller", isLoggedIn, async (req, res, next) => {
     }
   } catch (error) {
     console.error(error);
-    next(error);
   }
 });
 
 router.post("/", isLoggedIn, async (req, res, next) => {
   try {
-    const { productName, content, category, price, imgUrls } = req.body;
-
+    const { productName, content, category, price } = req.body;
     const product = await Product.create({
       sellerId: req.user.id,
       productName: productName,
@@ -71,33 +54,49 @@ router.post("/", isLoggedIn, async (req, res, next) => {
       category: category,
       price: price,
     });
-    console.log("이미지 urls", imgUrls);
-    if (imgUrls) {
-      imgUrls.data.map((url) => product.createProductImg({ imgUrl: url }));
-    }
-    return res.status(201).send("상품 등록 완료");
+    return res.status(201).send({ productId: product.id });
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
 
+router.post(
+  "/images/:id",
+  isLoggedIn,
+  multer.upload.array("productImgs", 10),
+  async (req, res) => {
+    const images = req.files;
+    const productId = req.params.id;
+    if (images) {
+      const locations = await images.map((img) => img.location);
+      locations.map((url) =>
+        ProductImg.create({ productId: productId, imgUrl: url })
+      );
+      return res.status(201).send(locations);
+    }
+    res.status(400).send("이미지가 없습니다.");
+  }
+);
+
 router.get(
   "/management",
   isLoggedIn,
-  upload.array("productImgs", 10, async (req, res, next) => {
+  multer.upload.array("productImgs", 10, async (req, res, next) => {
     try {
+      const sellerId = req.user.id;
       const limit = Number(req.query.limit);
       const pageNumber = req.query.page;
       const offset = 0 + limit * (pageNumber - 1);
-      const sellerId = req.user.id;
+
       const products = await Product.findAll({
         where: { sellerId: sellerId },
         attributes: ["productName", "status", "price", "updatedAt"],
         offset: offset,
         limit: limit,
       });
-      if (products) res.send(products);
+
+      if (products) res.status(200).send(products);
     } catch (error) {
       console.error(error);
       next(error);
@@ -105,23 +104,31 @@ router.get(
   })
 );
 
-router.delete("/:id", isLoggedIn, async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   try {
     const product = await Product.findOne({
-      attributes: ["id"],
+      attributes: ["id", "sellerId"],
       where: { id: req.params.id },
     });
     if (product) {
       const seller = await product.getUser();
-      if (seller) {
-        if (seller.id == req.user.id) {
-          await Product.destroy({ where: { id: product.id } });
-          res.status(200).send("완료");
-        }
+      if (seller.id) {
+        const productImgs = await ProductImg.findAll({
+          where: { productId: product.id },
+        });
+        const filenames = productImgs.map((img) =>
+          img.imgUrl.split("/").at(-1)
+        );
+        // s3에 있는 이미지들 삭제
+        filenames.map((file) => multer.delete_file(file));
+        await ProductImg.destroy({ where: { productId: product.id } });
+        await product.destroy();
+        res.status(200).send("완료");
       }
     }
   } catch (error) {
     console.log(error);
   }
 });
+
 module.exports = router;
--- src/multers.js
+++ src/multers.js
@@ -4,15 +4,38 @@ const aws = require("aws-sdk");
 aws.config.loadFromPath(__dirname + "/../config/s3.json");
 
 const s3 = new aws.S3();
-const upload = multer({
+module.exports.upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: "sylimgs",
     acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`);
+    key: function (req, file, callback) {
+      const productId = req.params.id;
+      callback(
+        null,
+        `productImgs/${productId}_${file.originalname}_${Date.now()}`
+      );
     },
   }),
 });
-module.exports = upload;
+module.exports.delete_file = (file) => {
+  try {
+    s3.deleteObject(
+      {
+        Bucket: "sylimgs",
+        Key: `productImgs/${file}`,
+      },
+      function (error, data) {
+        if (error) {
+          console.log("err: ", error, error.stack);
+        } else {
+          console.log(data, " 정상 삭제 되었습니다.");
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
